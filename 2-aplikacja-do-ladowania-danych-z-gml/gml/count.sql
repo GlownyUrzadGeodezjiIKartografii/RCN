6b932cb03b2d359f7a79fb1653896bf045e85099
GlownyUrzadGeodezjiIKartografii <127184532+GlownyUrzadGeodezjiIKartografii@users.noreply.github.com>
2026-08-26 07:47:42 +0200

    Update count.sql

--- 2-aplikacja-do-ladowania-danych-z-gml/gml/count.sql
+++ 2-aplikacja-do-ladowania-danych-z-gml/gml/count.sql
@@ -17,20 +17,22 @@ SELECT 'Transakcje', COUNT(*)
 FROM uslugi_rcn.transakcja;
 
 
+-- Oczekiwane wyniki kontrolne
+
 -- 1864-1-bazowy.zip
-"Działki"	3361
-"Budynki"	1618
-"Lokale"	765
-"Transakcje"	1696
+-- Działki:      3361
+-- Budynki:      1618
+-- Lokale:        765
+-- Transakcje:   1696
 
 -- 1864-2-przyrostowy.zip
-"Działki"	19
-"Budynki"	3
-"Lokale"	2
-"Transakcje"	5
+-- Działki:        19
+-- Budynki:         3
+-- Lokale:          2
+-- Transakcje:      5
 
--- razem
-"Działki"	3380
-"Budynki"	1621
-"Lokale"	767
-"Transakcje"	1701
+-- Razem po imporcie obu plików
+-- Działki:      3380
+-- Budynki:      1621
+-- Lokale:        767
+-- Transakcje:   1701
